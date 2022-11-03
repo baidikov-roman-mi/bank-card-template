4b66afa6a559850af0a38a37255aabe3a4fe68fb
Faded-User <wolkdem@gmail.com>
2022-11-03 22:31:53 +0200

    cardcheck v1.0 . Need to correct cvv

---
+++ index.js
@@ -0,0 +1,49 @@
+const number = document.querySelector("#number");
+const holder = document.querySelector("#holder");
+const mono = document.querySelectorAll(".mono");
+const year = document.querySelector("#year");
+const month = document.querySelector("#month");
+const cvv = document.querySelector("#cvv");
+const cards = document.querySelectorAll(".cards");
+const logo = document.querySelector(".pay-s-logo");
+number.oninput = numberChange;
+holder.oninput = holderChange;
+month.oninput = monthChange;
+year.oninput = yearChange;
+cvv.oninput = cvvChange;
+function numberChange(cvv) {
+  number.value = number.value
+    .replace(/(\d{4})(?!\s|$)/gm, `$1 `)
+    .match(/(?:\d{4} ?){0,3}(?:\d{0,4})?/);
+  mono[0].innerHTML = number.value;
+  let anyString = mono[0].innerHTML;
+  if (anyString.substring(0, 1) == 5) {
+    cards[0].classList.add("active");
+    logo.src = "./img/mastercard.png";
+  } else if (anyString.substring(0, 1) == 4) {
+    cards[1].classList.add("active");
+    logo.src = "./img/visa.png";
+  } else if (anyString.substring(0, 2) == 34) {
+    cards[2].classList.add("active");
+    logo.src = "./img/amex.png";
+  } else {
+    logo.src = "";
+    cards[0].classList.remove("active");
+    cards[1].classList.remove("active");
+    cards[2].classList.remove("active");
+  }
+}
+function holderChange() {
+  holder.value = holder.value.replace(/\d/g, "");
+  mono[1].innerHTML = holder.value;
+}
+function monthChange() {
+  mono[2].innerHTML = month.value + "/" + year.value;
+}
+function yearChange() {
+  mono[2].innerHTML = month.value + "/" + year.value;
+}
+function cvvChange() {
+  cvv.value = cvv.value.replace(/\D/g, "");
+  mono[3].innerHTML = cvv.value;
+}
